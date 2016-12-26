7edca46f2830a7033640b5741861334f5e17b045
zin <romandozin@gmail.com>
2016-12-26 13:09:19 +0100

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@ class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
+def index
     @groups = Group.all
   end
 
@@ -25,22 +26,23 @@ end
     else
       render :new
     end
+  end
 
   def update
-      if @group.update(group_params)
-        redirect_to groups_path, notice: "Update Success"
-      else
-        render :edit
-      end
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
     end
+  end
 
-    def destroy
-        @group.destroy
-        redirect_to groups_path, alert: "Group deleted"
-      end
+def destroy
+  @group.destroy
+  redirect_to groups_path, alert: "Group deleted"
+end
 
-     private
-     def find_group_and_check_permission
+  private
+  def find_group_and_check_permission
     @group = Group.find(params[:id])
 
     if current_user != @group.user
@@ -48,11 +50,9 @@ end
     end
   end
 
+
   def group_params
     params.require(:group).permit(:title, :description)
   end
 
-     def group_params
-       params.require(:group).permit(:title, :description)
-     end
 end
